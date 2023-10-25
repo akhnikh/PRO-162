4bf1e4208a7d842f3299fe552cdd8ffc4883fdfb
akhnikh <114444837+akhnikh@users.noreply.github.com>
2023-10-25 17:36:04 -0500

    Add files via upload

---
+++ shoot.js
@@ -0,0 +1,88 @@
+AFRAME.registerComponent("bullets", {
+  init: function () {
+    this.shootBullet();
+  },
+  shootBullet: function () {
+    window.addEventListener("keydown", (e) => {
+      if (e.key === "z") {
+        var bullet = document.createElement("a-entity");
+
+        bullet.setAttribute("geometry", {
+          primitive: "sphere",
+          radius: 0.1,
+        });
+
+        bullet.setAttribute("material", "color", "black");
+
+        var cam = document.querySelector("#camera");
+
+        pos = cam.getAttribute("position");
+
+        bullet.setAttribute("position", {
+          x: pos.x,
+          y: pos.y,
+          z: pos.z,
+        });
+
+        var camera = document.querySelector("#camera").object3D;
+
+        //get the camera direction as Three.js Vector
+        var direction = new THREE.Vector3();
+        camera.getWorldDirection(direction);
+
+        //set the dynamic property
+        bullet.setAttribute("dynamic-body", {
+          mass:0,
+          shape:"sphere",
+        })
+
+        //set the velocity and it's direction
+        bullet.setAttribute("velocity", direction.multiplyScalar(-10));
+
+        var scene = document.querySelector("#scene");
+
+        scene.appendChild(bullet);
+      }
+    });
+  },
+
+  removeBullet: function (e) {
+    //Original entity (bullet)
+    console.log(e.detail.target.el);
+
+    //Other entity, which bullet touched.
+    console.log(e.detail.body.el);
+
+    //bullet element
+    var element = e.detail.target.el; //bullet
+
+    //element which is hit
+    var elementHit = e.detail.body.el;
+
+    if (elementHit.id.includes("box")) 
+      {
+        //set material attribute
+        elementHit.setAttribute("material", {
+          opacity: 1, 
+          transparent: true,
+        })
+        
+
+        //impulse and point vector
+        // applyImpulse(impulse, worldPoint)  'impulse' is the amount of  impulse added to the body and worldPoint means the point at which the force is applied
+        var impulse = new CANNON.Vec3(-2, 2, 1);
+        var worldPoint = new CANNON.Vec3.copy(elementHit.getAttribute("position"));
+        elementHit.body.applyImpulse(impulse, worldPoint);
+
+        //remove event listener
+        element.removeEventListener('collide', this.shoot);
+        
+        //remove the bullets from the scene
+        var scene = document.querySelector("#scene");
+        scene.removeChild(element);
+      
+    }
+  },
+});
+
+
